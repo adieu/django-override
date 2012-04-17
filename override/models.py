2c9f25a077b7059c1eaf55b50a455a2f978273f5
adieu <adieu@adieu.cn>
2012-04-18 01:41:09 +0800

    bugfix

--- override/models.py
+++ override/models.py
@@ -3,4 +3,4 @@ from override.settings import MAKE_OVERRIDE_TAGS_BUILT_IN
 
 if MAKE_OVERRIDE_TAGS_BUILT_IN:
 	from django.template.base import add_to_builtins
-	add_to_builtins('override.templatetags.override')
+	add_to_builtins('override.templatetags.override_tags')
