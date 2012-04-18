8f6c4f265375f072e7b7300371fde75267d40164
adieu <adieu@adieu.cn>
2012-04-18 10:49:27 +0800

    replace tabs with space

--- override/models.py
+++ override/models.py
@@ -2,5 +2,5 @@ from override.settings import MAKE_OVERRIDE_TAGS_BUILT_IN
 
 
 if MAKE_OVERRIDE_TAGS_BUILT_IN:
-	from django.template.base import add_to_builtins
-	add_to_builtins('override.templatetags.override_tags')
+    from django.template.base import add_to_builtins
+    add_to_builtins('override.templatetags.override_tags')
